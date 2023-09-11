09db3e06fde4bf2b9a3ecd16cc694bb5f4ab979b
Deonis Butuc <deonis.butuc@endava.com>
2023-09-11 20:02:36 +0300

    Variabile si Comentarii

---
+++ Lesson1.1/src/main/java/org/example/Lesson01.java
@@ -0,0 +1,17 @@
+public class Lesson01 {
+    public static void main(String[] args) {
+/*Tema pentru acasa:
+declarare si initializare*/
+
+            byte byteName = 25;
+            short shortName = 250;
+            int intName = 2500;
+            long longName = 2555550;
+            boolean booleanName = true;
+            char charName = 'A';
+            float floatName = 25.87f;
+            double doubleName = 5258.566565;
+
+
+        }
+}
